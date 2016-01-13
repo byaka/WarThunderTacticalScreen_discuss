eff528f6357e815cc61e75b644844c2a9e9c720d
BYaka <byaka.life@gmail.com>
2016-01-14 00:46:14 +0300

    Create mapObjects.js

---
+++ API/mapObjects.js
@@ -0,0 +1,598 @@
+#http://localhost:8111/map_obj.json
+
+[
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.988334,
+      "dy": 0.152305,
+      "icon": "Assault",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.442905,
+      "y": 0.507544
+   },
+   {
+      "blink": 0,
+      "color": "#ff0D00",
+      "color[]": [
+         255,
+         13,
+         0
+      ],
+      "dx": -0.895805,
+      "dy": -0.444448,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.537559,
+      "y": 0.483042
+   },
+   {
+      "blink": 0,
+      "color": "#e10B00",
+      "color[]": [
+         225,
+         11,
+         0
+      ],
+      "dx": -0.999214,
+      "dy": 0.039650,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.540217,
+      "y": 0.494024
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.999272,
+      "dy": -0.038146,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.458232,
+      "y": 0.505343
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.817628,
+      "dy": -0.575747,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.453375,
+      "y": 0.486849
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.701607,
+      "dy": 0.712564,
+      "icon": "Assault",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.446549,
+      "y": 0.513330
+   },
+   {
+      "blink": 0,
+      "color": "#e10B00",
+      "color[]": [
+         225,
+         11,
+         0
+      ],
+      "dx": -0.738663,
+      "dy": 0.674075,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.544930,
+      "y": 0.508896
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.879526,
+      "dy": -0.475851,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.452031,
+      "y": 0.499593
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.856143,
+      "dy": -0.516739,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.462464,
+      "y": 0.500086
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 1.000000,
+      "dy": -0.000000,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.449265,
+      "y": 0.502838
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.896512,
+      "dy": -0.443019,
+      "icon": "Bomber",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.409614,
+      "y": 0.515963
+   },
+   {
+      "blink": 0,
+      "color": "#ff0D00",
+      "color[]": [
+         255,
+         13,
+         0
+      ],
+      "dx": -0.997988,
+      "dy": -0.063400,
+      "icon": "Bomber",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.542998,
+      "y": 0.492304
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.956681,
+      "dy": 0.291139,
+      "icon": "Bomber",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.401954,
+      "y": 0.521545
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.682609,
+      "dy": 0.730784,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.450053,
+      "y": 0.522021
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": -0.036655,
+      "dy": -0.999328,
+      "icon": "Player",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.393540,
+      "y": 0.504886
+   },
+   {
+      "blink": 0,
+      "color": "#e10B00",
+      "color[]": [
+         225,
+         11,
+         0
+      ],
+      "dx": -0.440393,
+      "dy": -0.897805,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.550280,
+      "y": 0.480142
+   },
+   {
+      "blink": 0,
+      "color": "#e10B00",
+      "color[]": [
+         225,
+         11,
+         0
+      ],
+      "dx": -0.576313,
+      "dy": -0.817229,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.552190,
+      "y": 0.485188
+   },
+   {
+      "blink": 0,
+      "color": "#e10B00",
+      "color[]": [
+         225,
+         11,
+         0
+      ],
+      "dx": -0.958019,
+      "dy": 0.286706,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.546850,
+      "y": 0.496816
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.669423,
+      "dy": 0.742881,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.450439,
+      "y": 0.520836
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 0.599271,
+      "dy": 0.800546,
+      "icon": "Fighter",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.443588,
+      "y": 0.521302
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.389237,
+      "y": 0.423428
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.417048,
+      "y": 0.414641
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.381931,
+      "y": 0.426403
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.380597,
+      "y": 0.435913
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.369193,
+      "y": 0.429165
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.373344,
+      "y": 0.419895
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.364951,
+      "y": 0.422822
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.422473,
+      "y": 0.421662
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.423792,
+      "y": 0.429964
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.411714,
+      "y": 0.407587
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.370730,
+      "y": 0.457759
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.378687,
+      "y": 0.464045
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.391277,
+      "y": 0.466697
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.401703,
+      "y": 0.466904
+   },
+   {
+      "blink": 0,
+      "color": "#ffFFFA",
+      "color[]": [
+         255,
+         255,
+         250
+      ],
+      "icon": "capture_zone",
+      "icon_bg": "none",
+      "type": "capture_zone",
+      "x": 0.500000,
+      "y": 0.500000
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 295.441986,
+      "dy": -52.094501,
+      "icon": "respawn_base_bomber",
+      "icon_bg": "none",
+      "type": "respawn_base_bomber",
+      "x": 0.379781,
+      "y": 0.521198
+   },
+   {
+      "blink": 0,
+      "color": "#ff0D00",
+      "color[]": [
+         255,
+         13,
+         0
+      ],
+      "dx": -295.441986,
+      "dy": 52.094501,
+      "icon": "respawn_base_bomber",
+      "icon_bg": "none",
+      "type": "respawn_base_bomber",
+      "x": 0.620205,
+      "y": 0.478808
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "dx": 298.858002,
+      "dy": -26.146700,
+      "icon": "respawn_base_fighter",
+      "icon_bg": "none",
+      "type": "respawn_base_fighter",
+      "x": 0.424004,
+      "y": 0.506651
+   },
+   {
+      "blink": 0,
+      "color": "#ff0D00",
+      "color[]": [
+         255,
+         13,
+         0
+      ],
+      "dx": -298.858002,
+      "dy": 26.146700,
+      "icon": "respawn_base_fighter",
+      "icon_bg": "none",
+      "type": "respawn_base_fighter",
+      "x": 0.575938,
+      "y": 0.493391
+   }
+]
