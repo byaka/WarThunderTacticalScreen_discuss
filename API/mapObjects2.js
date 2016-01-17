4f55434ba6611f4694ff78c80331eb3b93520ec3
BYaka <byaka.life@gmail.com>
2016-01-17 04:32:06 +0300

    Create mapObjects2.js

---
+++ API/mapObjects2.js
@@ -0,0 +1,654 @@
+[
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "ex": 0.492190,
+      "ey": 0.719701,
+      "icon": "none",
+      "icon_bg": "none",
+      "sx": 0.489432,
+      "sy": 0.717386,
+      "type": "airfield"
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "ex": 0.418728,
+      "ey": 0.768242,
+      "icon": "none",
+      "icon_bg": "none",
+      "sx": 0.415324,
+      "sy": 0.768626,
+      "type": "airfield"
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "ex": 0.496275,
+      "ey": 0.690167,
+      "icon": "none",
+      "icon_bg": "none",
+      "sx": 0.507301,
+      "sy": 0.679318,
+      "type": "airfield"
+   },
+   {
+      "blink": 0,
+      "color": "#60FF37",
+      "color[]": [
+         96,
+         255,
+         55
+      ],
+      "dx": -0.467876,
+      "dy": -0.883794,
+      "icon": "Player",
+      "icon_bg": "none",
+      "type": "aircraft",
+      "x": 0.468138,
+      "y": 0.694348
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.513652,
+      "y": 0.659012
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.514430,
+      "y": 0.658452
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512820,
+      "y": 0.659865
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.515000,
+      "y": 0.657379
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512334,
+      "y": 0.660123
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.502425,
+      "y": 0.661462
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.504186,
+      "y": 0.662276
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.501111,
+      "y": 0.660567
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.489857,
+      "y": 0.642989
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.493607,
+      "y": 0.637551
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.493199,
+      "y": 0.707524
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.479810,
+      "y": 0.691253
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512067,
+      "y": 0.661870
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512329,
+      "y": 0.661793
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.511803,
+      "y": 0.661918
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.520961,
+      "y": 0.636782
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.520840,
+      "y": 0.636702
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.521102,
+      "y": 0.636901
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.520737,
+      "y": 0.636549
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.521198,
+      "y": 0.636971
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.493470,
+      "y": 0.665749
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.493982,
+      "y": 0.666711
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.492625,
+      "y": 0.665323
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512299,
+      "y": 0.663454
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512560,
+      "y": 0.663369
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512254,
+      "y": 0.662820
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.512112,
+      "y": 0.662439
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.502992,
+      "y": 0.662933
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.503194,
+      "y": 0.662640
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.502756,
+      "y": 0.663265
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.503415,
+      "y": 0.662304
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.408947,
+      "y": 0.766705
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.411442,
+      "y": 0.772154
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Ship",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.390525,
+      "y": 0.770688
+   },
+   {
+      "blink": 1,
+      "color": "#f40C00",
+      "color[]": [
+         244,
+         12,
+         0
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.546204,
+      "y": 0.707468
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.507113,
+      "y": 0.690350
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.506508,
+      "y": 0.689824
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.505798,
+      "y": 0.689176
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.506165,
+      "y": 0.684056
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.506300,
+      "y": 0.683977
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.506031,
+      "y": 0.684160
+   },
+   {
+      "blink": 0,
+      "color": "#1952FF",
+      "color[]": [
+         25,
+         82,
+         255
+      ],
+      "icon": "Wheeled",
+      "icon_bg": "none",
+      "type": "ground_model",
+      "x": 0.506446,
+      "y": 0.683829
+   }
+]
