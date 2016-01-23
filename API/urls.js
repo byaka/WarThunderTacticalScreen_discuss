b8ecb58a4b204e3f7ba5716b5e1eea037875548b
BYaka <byaka.life@gmail.com>
2016-01-23 14:17:08 +0300

    Update urls.js

--- API/urls.js
+++ API/urls.js
@@ -2,3 +2,4 @@ var mapImage='http://localhost:8111/map.img'; //optionally u can pass "?gen=%s"
 var mapObjects='http://localhost:8111/map_obj.json';
 var state='http://localhost:8111/state';
 var indicators='http://localhost:8111/indicators';
+var mission='http://localhost:8111/mission.json';
