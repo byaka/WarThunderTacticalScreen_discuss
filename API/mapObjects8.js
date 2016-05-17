34fbaa6e0c095ca98ccdb8294772c69c39d1eb42
BYaka <byaka.life@gmail.com>
2016-05-17 18:51:40 +0300

    Create mapObjects8.js

---
+++ API/mapObjects8.js
@@ -0,0 +1,11 @@
+[
+{"type":"aircraft","color":"#60FF37","color[]":[96,255,55],"blink":0,"icon":"Player","icon_bg":"none","x":0.387853,"y":0.542026,"dx":0.046484,"dy":-0.998919},
+{"type":"ground_model","color":"#ff0D00","color[]":[255,13,0],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.398538,"y":0.546458},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.357526,"y":0.512372},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.359391,"y":0.506091},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.366421,"y":0.499567},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.366015,"y":0.500411},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.362918,"y":0.507685},
+{"type":"ground_model","color":"#1952FF","color[]":[25,82,255],"blink":0,"icon":"Airdefence","icon_bg":"none","x":0.360918,"y":0.505534},
+{"type":"waypoint","color":"#ffFFFF","color[]":[255,255,255],"blink":0,"icon":"waypoint","icon_bg":"none","x":0.388446,"y":0.532962},
+{"type":"waypoint","color":"#ffFFFF","color[]":[255,255,255],"blink":0,"icon":"waypoint","icon_bg":"none","x":0.394466,"y":0.525174}]
